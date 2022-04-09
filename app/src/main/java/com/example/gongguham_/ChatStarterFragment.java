7d053f2ee11b4bb2b720aa0e0058d34403bf456a
SJG <shinjg97@gmail.com>
2022-04-09 18:34:29 +0900

    1. 채팅 기능 문제 해결(2020-04-06) 2. <us branch> merge to mine 3. 방폭구현중

--- app/src/main/java/com/example/gongguham_/ChatStarterFragment.java
+++ app/src/main/java/com/example/gongguham_/ChatStarterFragment.java
@@ -1,13 +1,11 @@
 package com.example.gongguham_;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,14 +15,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -34,7 +30,6 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.w3c.dom.Text;
 
 
 public class ChatStarterFragment extends Fragment {
@@ -45,10 +40,12 @@ public class ChatStarterFragment extends Fragment {
     private EditText chat_name;
     private TextView user_name;
     private Button user_next;
+    private Button boom_button;
     private ListView chat_list;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
 
 
     public ChatStarterFragment() {
@@ -81,7 +78,10 @@ public class ChatStarterFragment extends Fragment {
         final TextView name = view.findViewById(R.id.user_name);
 
         user_next = (Button) view.findViewById(R.id.user_next);
+        boom_button = (Button) view.findViewById(R.id.boom_button);
+
         chat_list = (ListView) view.findViewById(R.id.chat_list);
+
 
         DocumentReference documentReference = FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getEmail());
         documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -92,6 +92,7 @@ public class ChatStarterFragment extends Fragment {
                     if (document != null) {
                         if (document.exists()) {
                             name.setText(document.getData().get("name").toString());
+                            //name.setText(MemberInfo.class.getName());
                         } else {
                             Log.e(TAG, "No such document");
                         }
@@ -105,14 +106,25 @@ public class ChatStarterFragment extends Fragment {
         user_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 공백 입력시
                 if(name.getText().toString().equals("") || chat_name.getText().toString().equals("")){
                     Toast.makeText(getContext(),"채팅방 이름을 입력해주세요.",Toast.LENGTH_SHORT).show();
-                }else{
+                } else{ //방 입장
                     Toast.makeText(getContext(),"채팅방에 입장하였습니다.",Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(getContext(), ChatChattingActivity.class);
                     intent.putExtra("chatName", chat_name.getText().toString());
                     intent.putExtra("userName", name.getText().toString());
                     startActivity(intent);
+                }
+            }
+        });
+        boom_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().equals("") || chat_name.getText().toString().equals("")){
+                    Toast.makeText(getContext(),"채팅방 이름을 입력해주세요.",Toast.LENGTH_SHORT).show();
+                }else{ //방폭ㅅ
+                    Toast.makeText(getContext(), "채팅방이 터졌습니다.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -130,7 +142,6 @@ public class ChatStarterFragment extends Fragment {
 
                 = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, android.R.id.text1);
         chat_list.setAdapter(adapter);
-
         // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
         databaseReference.child("chat").addChildEventListener(new ChildEventListener() {
             @Override
@@ -146,7 +157,8 @@ public class ChatStarterFragment extends Fragment {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-
+                Log.e("LOG", "dataSnapshot.getKey() : " + dataSnapshot.getKey());
+                adapter.remove(dataSnapshot.getKey());
             }
 
             @Override
@@ -166,14 +178,14 @@ public class ChatStarterFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 chat_name.setText(adapter.getItem(i));
+                Intent intent = new Intent(getContext(), ChatChattingActivity.class);
+                intent.putExtra("chatName", chat_name.getText().toString());
+                startActivity(intent);
             }
         });
     }
-    private  void startMainActivity(){
-        Intent intent=new Intent(getContext(),ChatChattingActivity.class);
-        intent.putExtra("chatName", "chicken");
-        intent.putExtra("userName", "kim");
-        startActivity(intent);
+    private void boomRoom(){
+
     }
 
 
