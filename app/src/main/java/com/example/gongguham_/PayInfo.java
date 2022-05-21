e19aec48b81150778ca18ee346e198f3c39af5cf
Kyeongnam Kim <nam433815@gmail.com>
2022-05-22 03:11:01 +0900

    포인트 충전, 송금, 환급 구현

--- app/src/main/java/com/example/gongguham_/PayInfo.java
+++ app/src/main/java/com/example/gongguham_/PayInfo.java
@@ -1,12 +1,12 @@
 package com.example.gongguham_;
 
 public class PayInfo {
-    private String point;
+    private int point;
 
-    PayInfo(String point) {
+    PayInfo(int point) {
         this.point = point;
     }
 
-    public String getPoint(){ return this.point; }
-    public void setPoint(String point){ this.point = point; }
+    public int getPoint(){ return this.point; }
+    public void setPoint(int point){ this.point = point; }
 }
